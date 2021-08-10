4aa3572338842cc277ee6a0730f0094a180653f6
Ejim Michael <ejimmichael1@gmail.com>
2021-08-10 16:07:51 +0100

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAGCcJHUpXNCOqzgDAwe4EhG40df9pLEUY",
+    authDomain: "whatsapp-clone-a695d.firebaseapp.com",
+    projectId: "whatsapp-clone-a695d",
+    storageBucket: "whatsapp-clone-a695d.appspot.com",
+    messagingSenderId: "796470560309",
+    appId: "1:796470560309:web:53d4a4751c5787f7cb537c"
+  };
