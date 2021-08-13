f8edd34116e72df7518dddcb820bc28f988846b5
Ejim Michael <ejimmichael1@gmail.com>
2021-08-13 15:13:38 +0100

    style whatsapp login page

--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 import "./Login.css"
+import { auth, provider } from './firebase';
 
 function Login() {
-    const signIn = () => {};
+    const signIn = () => {
+        auth.signInWithPopup(provider).then()
+    };
     return (
         <div className="login">
             <div className="login__container">
-                <img src="" alt="" />
+                <img src="https://www.logo.wine/a/logo/WhatsApp/WhatsApp-Logo.wine.svg" alt="WhatsApp img" />
                 <div className="login__text">
-                    <h1>Sign in to whatsApp</h1>
+                    <h1>Sign in to WhatsApp</h1>
                 </div>
 
                 <Button onClick={signIn}>Sign In With Google</Button>
