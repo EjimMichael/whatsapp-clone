04dab64274f31b08a410377b17637b63b01b2387
Ejim Michael <ejimmichael1@gmail.com>
2021-08-15 10:39:25 +0100

    setup google authentication

--- src/Login.js
+++ src/Login.js
@@ -5,8 +5,11 @@ import { auth, provider } from './firebase';
 
 function Login() {
     const signIn = () => {
-        auth.signInWithPopup(provider).then()
-    };
+        auth.signInWithPopup(provider)
+        .then((result) => console.log(result))
+        .catch((error) => alert(error.message));
+    }
+
     return (
         <div className="login">
             <div className="login__container">
